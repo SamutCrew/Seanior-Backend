303ec7e074f69518dd91136891257aa8b29a4ed9
Kollawat Ruanghirun <eark050346@gmail.com>
2025-05-07 21:34:19 +0700

    add api for motificatiion feature

--- seanior-backend/src/instructor-request/instructor-request.module.ts
+++ seanior-backend/src/instructor-request/instructor-request.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { InstructorRequestService } from './instructor-request.service';
 import { InstructorRequestController } from './instructor-request.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { NotificationService } from '../notification/notification.service';
 
 @Module({
   controllers: [InstructorRequestController],
-  providers: [InstructorRequestService, PrismaService],
+  providers: [InstructorRequestService, PrismaService, NotificationService],
 })
 export class InstructorRequestModule {}
